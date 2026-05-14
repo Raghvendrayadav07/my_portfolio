bd8da2d1fac7149c2fffdd89da5d684936f9bfcf
Raghvendra Yadav <119030170+Raghvendrayadav07@users.noreply.github.com>
2026-05-14 07:39:24 +0200

    Initial portfolio deployment

---
+++ application.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+# EB requires the app object to be named 'application'
+application = Flask(__name__)
+
+@application.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    application.run(debug=False)
